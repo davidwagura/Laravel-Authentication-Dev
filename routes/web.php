cd2b89708a9ae717b926cb31507baa659f7d89e5
David wanjohi <davidwagura83@gmail.com>
2024-03-01 12:06:17 +0300

    apply policy of viewing an order

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('order/{order}', function (\App\Models\Order $order) {
     return view('order.view', ['order' => $order]);
 })
@@ -20,4 +9,5 @@ Route::get('order/{order}', function (\App\Models\Order $order) {
     ->middleware([
         'auth.signed:order,customer',
         'auth:customer,seller',
+        'can:view,order'
     ]);
